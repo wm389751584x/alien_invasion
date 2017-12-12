f1964cd2c08a3dc8c0da3a996d60469ea6c86d30
Jason Wang <wangmenghw@gmail.com>
2017-12-11 18:47:13 -0600

    Working Commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -8,8 +9,11 @@ def run_game():
     pygame.init()
     ai_settings = Settings()
 
-    screen = pygame.display.set_mode(ai_settings.screen_width, ai_settings.screen_height)
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship.
+    ship = Ship(screen)
 
     # Start the main loop for the game
     while True:
@@ -21,6 +25,7 @@ def run_game():
 
         # redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
