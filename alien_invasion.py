6b527c989add69ab5d402629205871032bc4acb7
Jason Wang <wangmenghw@gmail.com>
2017-12-27 12:49:45 -0600

    full commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,8 +13,7 @@ def run_game():
     pygame.init()
     ai_settings = Settings()
 
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # Make a play button.
